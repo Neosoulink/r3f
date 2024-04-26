4569be4b8af1940bfdeb87e7b16054cf3de86f16
Nathan Mande <neodarksoulink@gmail.com>
2024-04-26 11:36:22 +0200

    feat:  add `leva` support

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./experience";
+import { Leva } from "leva";
 
 export const App = () => {
 	return (
-		<Canvas>
-			<Experience />
-		</Canvas>
+		<>
+			<Leva collapsed />
+
+			<Canvas>
+				<Experience />
+			</Canvas>
+		</>
 	);
 };
--- src/experience.tsx
+++ src/experience.tsx
@@ -1,31 +1,38 @@
-import {
-	Float,
-	MeshReflectorMaterial,
-	OrbitControls,
-	Text,
-} from "@react-three/drei";
+import { Float, OrbitControls, Text } from "@react-three/drei";
+import { useControls } from "leva";
+import { Perf } from "r3f-perf";
 
 export const Experience = () => {
+	const levaControls = useControls({
+		textPosition: { value: { x: 0, y: 1, z: 1 }, step: 0.01 },
+		textColor: "#00ff00",
+	});
+
 	return (
 		<>
+			<Perf position="top-left" />
+
 			<OrbitControls makeDefault />
 			<ambientLight intensity={1.5} />
 			<directionalLight position={[1, 2, 3]} intensity={4.5} />
 
 			<Float>
-				<Text position={[0, 1, 0]} textAlign="center">
+				<Text
+					position={[
+						levaControls.textPosition.x,
+						levaControls.textPosition.y,
+						levaControls.textPosition.z,
+					]}
+					textAlign="center"
+					color={levaControls.textColor}
+				>
 					R3F
 				</Text>
 			</Float>
 
 			<mesh position={[0, -1, 0]} rotation={[-Math.PI * 0.5, 0, 0]} scale={10}>
 				<planeGeometry />
-				<MeshReflectorMaterial
-					mirror={1}
-					resolution={512}
-					blur={[1000, 1000]}
-					mixBlur={0.8}
-				/>
+				<meshBasicMaterial color={levaControls.textColor} />
 			</mesh>
 		</>
 	);
